33e6a9061f1d311f45aae76187c103dbb5c81587
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2024-04-01 23:13:16 +0300

    fix import after merge

--- src/Services/DocumentationGenerator.php
+++ src/Services/DocumentationGenerator.php
@@ -2,6 +2,7 @@
 
 namespace romanzipp\ModelDoc\Services;
 
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model as IlluminateModel;
